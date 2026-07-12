b8ce84bc997edb866469b15829238fce1ba97080
Devexhhh <kumarsujal.codes@gmail.com>
2026-07-12 21:51:56 +0530

    1100

--- TLE Eliminators/1100/VideoPosts.cpp
+++ TLE Eliminators/1100/VideoPosts.cpp
@@ -7,22 +7,18 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
     int n, k;
     if (!(cin >> n >> k)) return 0;
-
     vector<int> a(n);
     int total_sum = 0;
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
         total_sum += a[i];
     }
-
     if (total_sum % k != 0) {
         cout << "No\n";
         return 0;
     }
-
     int target = total_sum / k;
     vector<int> segment_lengths;
     int current_sum = 0;
@@ -41,12 +37,10 @@ int main() {
             return 0;
         }
     }
-
     cout << "Yes\n";
     for (size_t i = 0; i < segment_lengths.size(); ++i) {
         cout << segment_lengths[i] << (i == segment_lengths.size() - 1 ? "" : " ");
     }
     cout << "\n";
-
     return 0;
 }
